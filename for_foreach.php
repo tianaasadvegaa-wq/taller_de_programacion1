aa482cb3a6f28ee4032dde2499efdc9d82b9b32b
tianaasadvegaa-wq <tiana.asad.vegaa@gmail.com>
2026-09-03 13:29:22 -0300

    Add files via upload

---
+++ for_foreach.php
@@ -0,0 +1,31 @@
+<?php
+
+//Ejemplo 1 incremento
+    for($A=1; $A<=20; $A++){
+        echo $A."<br>";
+    }
+
+echo "<br>";
+
+//Ejemplo 1 decremento
+for($B=20; $B>=1; $B--){
+        echo $B."<br>";
+    }
+
+echo "<br>";
+
+//Ejemplo 2 Incremento
+$numero=7;
+
+    for($n=1; $n<=12; $n++){
+        echo $numero." X ".$n." = ".$n*$numero."<br>";
+    }
+
+echo "<br>";
+
+//Ejemplo 2 decremento
+$numero=7;
+
+    for($n=12; $n>=1; $n--){
+        echo $numero." X ".$n." = ".$n*$numero."<br>";
+    }
