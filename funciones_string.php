c237f79378ef215438951fff24dcd85e052387c9
tianaasadvegaa-wq <tiana.asad.vegaa@gmail.com>
2026-09-12 17:35:28 -0300

    Add files via upload

--- funciones_string.php
+++ funciones_string.php
@@ -14,3 +14,11 @@ echo $cadena_texto." tiene ".$longitud." caracteres <br>";
 
 $palabras=str_word_count($cadena_texto);
 echo $cadena_texto." tiene ".$palabras." palabras <br>";
+
+//3
+$fecha_1="2010/05/19";
+$fecha_2="2010-04-27";
+$numeros="Uno Dos Tres Cuatro Cinco Sies Siete";
+
+$array_numeros=explode(" ",$numeros,-2);
+echo $array_numeros[4];
